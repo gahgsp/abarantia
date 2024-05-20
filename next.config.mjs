1378ed0e4fd1bcbd427cf882a5ef1114b71ec3e3
gahgsp <kelvinbianchi@gmail.com>
2024-05-20 14:01:24 +0200

    feat: removing unnecessary MDX libraries

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-import createMDX from '@next/mdx'
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    pageExtensions: ['md', 'ts', 'tsx']
-};
+const nextConfig = {};
 
-const withMDX = createMDX({})
-
-export default withMDX(nextConfig);
+export default nextConfig;
--- src/mdx-components.tsx
+++
@@ -1,7 +0,0 @@
-import type { MDXComponents } from "mdx/types";
-
-const useMDXComponents = (components: MDXComponents) => {
-  return { ...components };
-};
-
-export default useMDXComponents;
