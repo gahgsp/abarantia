60e2ff7c7f2423647abff56ffbbf94da457495b8
gahgsp <kelvinbianchi@gmail.com>
2025-03-13 21:37:17 +0100

    feat: created generic error page

---
+++ src/app/error.tsx
@@ -0,0 +1,7 @@
+// An Error Page (error.tsx) will always be displayed when an error happens.
+// We can have multiple files to act as a fallback in every route level.
+const Error = () => {
+  return <h2>Ops... something wrong happened!</h2>;
+};
+
+export default Error;
