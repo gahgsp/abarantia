6d5a1ed97944f2d3cbe15ec76aa3d40efe4709d7
gahgsp <kelvinbianchi@gmail.com>
2025-03-13 21:40:20 +0100

    feat: added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+// A Not Found Page (not-found.tsx) to be displayed when the URL is invalid.
+// As "error.tsx", "not-found.tsx" is a special named file.
+const NotFound = () => {
+  return <h2>This page does not exist! Please, fix your URL address.</h2>;
+};
+
+export default NotFound;
