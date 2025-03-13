c64a274f2b8d99b097626603aa2205e1126ce393
gahgsp <kelvinbianchi@gmail.com>
2025-03-13 21:39:15 +0100

    fix: missing use client

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,7 @@
 // An Error Page (error.tsx) will always be displayed when an error happens.
 // We can have multiple files to act as a fallback in every route level.
+"use client";
+
 const Error = () => {
   return <h2>Ops... something wrong happened!</h2>;
 };
